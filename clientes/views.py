c32d2375e479f7d25a41083a6311890e19d18b97
Matheus Bosa Mischiatti <matheusbosa95@gmail.com>
2023-11-26 09:22:55 -0300

    Commit 03

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from datetime import datetime
 from .models import Cliente, Carro
 import re
 from django.core import serializers
 import json
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 def clientes(request):
     if request.method == "GET":
@@ -44,5 +48,36 @@ def clientes(request):
 def att_cliente(request):
     id_cliente = request.POST.get('id_cliente')
     cliente = Cliente.objects.filter(id=id_cliente)
+    carros = Carro.objects.filter(cliente=cliente[0])
     cliente_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
-    return JsonResponse(cliente_json)
+    carros_json = json.loads(serializers.serialize('json', carros))
+    carros_json = [{'fields': i['fields'], 'id': i['pk']} for i in carros_json]
+    data = {'cliente': cliente_json, 'carros': carros_json}
+    return JsonResponse(data)
+
+@csrf_exempt
+def update_carro(request, id):
+    nome_carro = request.POST.get('carro')
+    placa = request.POST.get('placa')
+    ano = request.POST.get('ano')
+
+    carro = Carro.objects.get(id=id)
+    list_carros = Carro.objects.filter(placa=placa).exclude(id=id)
+
+    if list_carros.exists():
+        return HttpResponse('Placa já existente') 
+        
+    carro.carro = nome_carro
+    carro.placa = placa
+    carro.ano = ano
+    carro.save()
+
+    return HttpResponse(id)
+
+def excluir_carro(request, id):
+    try:
+        carro = Carro.objects.get(id=id)
+        carro.delete()
+        return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
+    except:
+        return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
